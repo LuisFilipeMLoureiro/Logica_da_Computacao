76641fe8b05acd92a9ef38625c122f558a2af2c3
Luís Filipe Martins Loureiro <luisfml@al.insper.edu.br>
2022-02-23 01:57:42 -0300

    .

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ for elements in sys.argv:
         arg.append(elements)
 
 word = "".join(arg)
-word = word.strip().strip("").strip('')
+word = word.replace(" ", "").strip("").strip('')
+
 
 numbers = ["1", "2", "3", "4", "5", "6","7", "8","9","0"]
 op = ["+","-"]
 num = "0"
 
-or_numb = []
+or_numb0 = []
 or_op = []
 
 if word[0] not in op:
@@ -22,22 +23,25 @@ if word[0] not in op:
         if i in numbers:
             num += i  
         elif i in op:
-            or_numb.append(int(num[1:]))
+            or_numb0.append(int(num[1:]))
             num = "0"
             or_op.append(i)
         else:
-             sys.stderr.write("This is error msg")
+             sys.stderr.write("This is error msg1")
 
 
     
-    or_numb.append(int(num))
+    or_numb0.append(int(num))
 
 else:
-    sys.stderr.write("This is error msg")
+    sys.stderr.write("This is error msg2")
             
 
+or_numb = []
 
-
+for i in or_numb0:
+    if i != 0:
+        or_numb.append(i)
 
 
 tot = or_numb[0]
@@ -59,8 +63,8 @@ for numero in or_numb[1:]:
 
 
 
-if len(or_numb) == 0 or len(or_op) == 0:
-    sys.stderr.write("This is error msg")
+if len(or_numb) == 0 or len(or_op) == 0 or len(or_numb[1:]) == 0:
+    sys.stderr.write("This is error msg3")
 else:
     print(tot)
 
