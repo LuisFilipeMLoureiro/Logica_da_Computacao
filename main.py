0cf6c512e9c0f0b8869987f988fb30cc3fa7cb2f
Luís Filipe Martins Loureiro <luisfml@al.insper.edu.br>
2022-02-23 00:58:07 -0300

    adding main

---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+from tkinter import W
+
+
+arg = []
+for elements in sys.argv:
+    if elements != "main.py":
+        arg.append(elements)
+
+word = "".join(arg)
+word = word.strip().strip("").strip('')
+
+numbers = ["1", "2", "3", "4", "5", "6","7", "8","9","0"]
+op = ["+","-"]
+num = "0"
+
+or_numb = []
+or_op = []
+
+if word[0] not in op:
+    for i in word:
+        if i in numbers:
+            num += i
+        elif i in op:
+            or_numb.append(int(num))
+            num = "0"
+            or_op.append(i)
+            
+
+    or_numb.append(int(num))
+
+tot = or_numb[0]
+t = 0
+for numero in or_numb[1:]:
+    if or_op[t] == "+":
+        tot += numero
+    else:
+        tot-=numero 
+
+    if t<len(or_op)-1:
+        t+=1
+
+
+
+
+print(tot)
+#for elemento in sys.argv[1]:
